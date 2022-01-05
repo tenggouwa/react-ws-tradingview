c1af28f053e43aeb4216f8cb3deedb2949c88475
tenggouwa <17681875268@163.com>
2022-01-05 18:20:21 +0800

    feat:change

--- src/components/commonHook/index.js
+++ src/components/commonHook/index.js
@@ -37,58 +37,55 @@ export default class Hook extends Component {
         }
         window.routerHistory = history
     }
-    componentWillReceiveProps(props) {
-        const { userAuth } = localStorage
-        if (props.userAuth === 0 || !props.userAuth) {
-            if (this.props.location !== props.location && JSON.stringify(props.userAuth) !== userAuth
-            ) {
-                this.props.dispatch(this.props.clearUserAuth())
-                this.props.dispatch(this.props.getUserAuth())
-            }
-        }
-        const { pathname, search } = props.location
+    // componentWillReceiveProps(props) {
+    //     const { userAuth } = localStorage
+    //     if (props.userAuth === 0 || !props.userAuth) {
+    //         if (this.props.location !== props.location && JSON.stringify(props.userAuth) !== userAuth
+    //         ) {
+    //             this.props.dispatch(this.props.clearUserAuth())
+    //             this.props.dispatch(this.props.getUserAuth())
+    //         }
+    //     }
+    //     const { pathname, search } = props.location
 
-        if (JSON.stringify(props.userAuth) === '0' &&
-            (pathname.startsWith('/community'))) {
-            this.props.dispatch(this.props.clearUserAuth())
-            localStorage.userAuth = 0
-            props.history.replace(`/login?backUrl=${encodeURIComponent(pathname)}${encodeURIComponent(search)}`)
-        }
-        // TODO
-        if (JSON.stringify(props.userAuth) !== '0' && (props.userAuth !== this.props.userAuth) && localStorage.sessionId) {
-            const paramAry = {
-                reqType: -3,
-                sessionId: localStorage.sessionId
-            }
-            props.dispatch(props.sendWs(props.wsObj, JSON.stringify(paramAry)))
-        }
-        if ((JSON.stringify(props.userAuth) !== '0' && (props.userAuth !== this.props.userAuth)) || (props.wsConnet === true && props.wsConnet !== this.props.wsConnet)) { // 对公共websocket处理，检测下登录状态，加上sessionId
-            const param = {
-                reqType: 12
-            }
-            props.dispatch(props.sendWs(props.wsObj, JSON.stringify(param)))
-        }
-        // if (props.userAuth && !props.userAuth.isFutureTradeAuth && pathname.startsWith('/trade/futures')) {
-        //     props.history.replace('/common/futures')
-        // }
-        // if (props.userAuth && props.userAuth.isFutureTradeAuth && localStorage.pathName.startsWith('/common/futures')) {
-        //     props.history.replace('/trade/futures')
-        // }
-        if (JSON.stringify(props.userAuth) !== '0' && (props.userAuth !== this.props.userAuth)) { // 当前是登录状态
-            if (search.indexOf('inviteCode') !== -1) { // 找到邀请码
-                props.history.replace('/invite')
-            }
-        }
-        // if (props.swith === true && pathname.startsWith('/trade')) {
-        //     props.history.replace('/tradingcontest')
-        // }
-        if (props.history !== this.props.history) {
-            window.routerHistory = this.props.history
-        }
-    }
-    componentWillUnmount() {
-        if (this.timer) clearInterval(this.timer)
-    }
+    //     if (JSON.stringify(props.userAuth) === '0' &&
+    //         (pathname.startsWith('/community'))) {
+    //         this.props.dispatch(this.props.clearUserAuth())
+    //         localStorage.userAuth = 0
+    //         props.history.replace(`/login?backUrl=${encodeURIComponent(pathname)}${encodeURIComponent(search)}`)
+    //     }
+    //     // TODO
+    //     if (JSON.stringify(props.userAuth) !== '0' && (props.userAuth !== this.props.userAuth) && localStorage.sessionId) {
+    //         const paramAry = {
+    //             reqType: -3,
+    //             sessionId: localStorage.sessionId
+    //         }
+    //         props.dispatch(props.sendWs(props.wsObj, JSON.stringify(paramAry)))
+    //     }
+    //     if ((JSON.stringify(props.userAuth) !== '0' && (props.userAuth !== this.props.userAuth)) || (props.wsConnet === true && props.wsConnet !== this.props.wsConnet)) { // 对公共websocket处理，检测下登录状态，加上sessionId
+    //         const param = {
+    //             reqType: 12
+    //         }
+    //         props.dispatch(props.sendWs(props.wsObj, JSON.stringify(param)))
+    //     }
+    //     // if (props.userAuth && !props.userAuth.isFutureTradeAuth && pathname.startsWith('/trade/futures')) {
+    //     //     props.history.replace('/common/futures')
+    //     // }
+    //     // if (props.userAuth && props.userAuth.isFutureTradeAuth && localStorage.pathName.startsWith('/common/futures')) {
+    //     //     props.history.replace('/trade/futures')
+    //     // }
+    //     if (JSON.stringify(props.userAuth) !== '0' && (props.userAuth !== this.props.userAuth)) { // 当前是登录状态
+    //         if (search.indexOf('inviteCode') !== -1) { // 找到邀请码
+    //             props.history.replace('/invite')
+    //         }
+    //     }
+    //     // if (props.swith === true && pathname.startsWith('/trade')) {
+    //     //     props.history.replace('/tradingcontest')
+    //     // }
+    //     if (props.history !== this.props.history) {
+    //         window.routerHistory = this.props.history
+    //     }
+    // }
     render() {
         return null
     }
