21a43667df6adc4b025498f2929f47470eb393e1
tenggouwa <17681875268@163.com>
2022-01-10 17:04:59 +0800

    feat: changed

--- src/components/commonKLine/index.js
+++ src/components/commonKLine/index.js
@@ -2,17 +2,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
-import { chooseContractName } from '@/assets/js/common'
-// import VConsole from 'vconsole';
 
 import { Datafeeds, extraConfig, ThemeDark, ThemeWhite } from './datafees'
 import { widget } from '../../../static/TradingView/charting_library';
 import './index.scss'
-
-// if (window.location.protocol === 'http:') {
-//   new VConsole();
-// }
-// new VConsole();
 
 function setupWebViewJavascriptBridge(callback) {
 	if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
@@ -26,23 +19,18 @@ function setupWebViewJavascriptBridge(callback) {
 }
 @withRouter
 @connect(state => ({
-    apis: state.apis,
-    theme: state.theme,
-    usdkData: state.usdkData,
-    contractData: state.contractData,
     lang: state.lang,
     ws7: state.wsData.ws_7,
     wsObj: state.wsObj,
     sendWs: state.sendWs,
     wsConnet: state.wsConnet,
     setLang: state.setLang,
-    getUsdkData: state.getUsdkData,
-    getContractData: state.getContractData,
 }))
 export default class index extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      webData: {},
       theme: ThemeWhite,
     }
     this.cacheData = {}
@@ -53,67 +41,59 @@ export default class index extends React.Component {
 	tvWidget = null;
 
   componentDidMount() {
-    const _that = this;
-    // _that.initTv('1', 9);
+    const mockData = {
+      type: '1',
+      klineId: '9',
+      resolution: '15',
+      theme: 'White',
+      lang: 'zh',
+      pre: 2,
+    }
 
-    this.initTradingview(this.props)
-    setupWebViewJavascriptBridge(function(bridge) {
-      bridge.registerHandler('tvInit', (data, responseCallback) => {
-        const {
-          type,
-          klineId,
-          resolution,
-          theme,
-          lang,
-        } = data;
-        console.log('getData', data);
-        _that.initTv(type, klineId);
-        if (_that.tvWidget) {
-          _that.tvWidget.chart().setResolution(resolution, function onReadyCallback() {});
-        } else {
-          _that.resolution = resolution;
-        }
-        _that.setState({
-          theme: theme === 'Dark' ? ThemeDark: ThemeWhite
-        })
-        _that.props.dispatch(_that.props.setLang(lang))
-        // const params = {
-        //   type: 1, // 1合约 2现货
-        //   klineId: 1, // assetId / contractId,
-        //   resolution: 1,
-        //   theme: 'Dark', // Dark / White
-        //   lang: 'en', 
-        // }
-      });
-    })
+    this.setState({
+      webData: mockData,
+      theme: mockData.theme === 'Dark' ? ThemeDark: ThemeWhite
+    }, () => {
+      const { resolution, lang, pre } = this.state.webData
+      this.tradePricePrecision = pre
+      if (this.tvWidget) {
+        this.tvWidget.chart().setResolution(resolution, function onReadyCallback() {});
+      } else {
+        this.resolution = resolution;
+      }
+      this.props.dispatch(this.props.setLang(lang))
+      this.initTradingview(this.props)
+    });
+
+    setTimeout(() => {
+      const a = document.getElementsByClassName('onlyOneButtonCanBeStick-2KhwsEwE')
+      console.log(a);
+    }, 1000)
+    // setupWebViewJavascriptBridge(function(bridge) {
+    //   bridge.registerHandler('tvInit', (data, responseCallback) => {
+    //     const {
+    //       type,
+    //       klineId,
+    //       resolution,
+    //       theme,
+    //       lang,
+    //     } = data;
+    //     console.log('getData', data);
+    //     _that.initTv(type, klineId);
+    //     if (_that.tvWidget) {
+    //       _that.tvWidget.chart().setResolution(resolution, function onReadyCallback() {});
+    //     } else {
+    //       _that.resolution = resolution;
+    //     }
+    //     _that.setState({
+    //       theme: theme === 'Dark' ? ThemeDark: ThemeWhite
+    //     })
+    //     _that.props.dispatch(_that.props.setLang(lang))
+    //   });
+    // })
   }
 
   componentWillReceiveProps(props) {
-    if (props.type === '2' && props.usdkData && props.usdkData !== this.props.usdkData) {
-      this.tradePricePrecision = props.usdkData.usdkTradePricePrecision
-      if (this.tvWidget) {
-        if (props.usdkData.assetName !== this.props.usdkData.assetName) {
-          this.setSymbolName(props.usdkData.assetName)
-        }
-        return
-      }
-      this.initTradingview(props)
-    }
-    console.log('1111', props.contractData)
-    // if (props.type === '1' && props.contractData && props.contractData !== this.props.contractData) {
-    //   console.log(22222)
-    //   this.tradePricePrecision = props.contractData.contractTradePricePrecision
-    //   if (this.tvWidget) {
-    //     console.log(33333)
-    //     console.log(9999, props.contractData, this.filterContractName(props));
-    //     if (this.filterContractName(props) !== this.filterContractName(this.props)) {
-    //       console.log(44444, this.filterContractName(props))
-    //       this.setSymbolName(this.filterContractName(props))
-    //     }
-    //     return
-    //   }
-    //   this.initTradingview(props)
-    // }
     if (props.ws7 && props.ws7 !== this.props.ws7) {
       this.formatKlineData(props.ws7)
     }
@@ -125,19 +105,6 @@ export default class index extends React.Component {
     }
   }
 
-  initTv = async (type, id) => {
-    if (type === '1') {
-      await this.props.dispatch(this.props.getContractData(id))
-    } else {
-      await this.props.dispatch(this.props.getUsdkData(id))
-    }
-  }
-
-  filterContractName = ({ contractData, lang }) => {
-    const isContract = contractData.contractType === 3
-    return chooseContractName(lang, isContract, contractData.symbol, contractData.contractName)
-  }
-  
 	initTradingview (props) {
     const { theme } = this.state;
     const { lang } = props || this.props
@@ -155,13 +122,11 @@ export default class index extends React.Component {
 		const widgetOptions = {
 			debug: !true,
       symbol: ' ',
-      // symbol: props.type === '2' ? props.usdkData.assetName : this.filterContractName(props),
       datafeed: Object.assign({}, Datafeeds, extraConfig(this)),
       interval: this.resolution,
       container_id: 'tv_chart_container',
       library_path: '/static/TradingView/charting_library/',
       timezone: 'Asia/Shanghai',
-      // custom_css_url: 'css/tradingview_night.css',
       locale: tvLang,
       fullscreen: false,
       autosize: true,
@@ -190,14 +155,10 @@ export default class index extends React.Component {
         'header_screenshot',
         'header_saveload',
         'display_market_status',
-        // 'legend_widget'
       ],
       enabled_features: [
         'left_toolbar',
         'hide_left_toolbar_by_default',
-      ],
-      context_menus: [
-        'legend_context_menu',
       ],
       time_frames: [],
       overrides: {
@@ -206,14 +167,12 @@ export default class index extends React.Component {
         'symbolWatermarkProperties.color': 'rgba(0,0,0,0)', // 水印的主要配置
         'paneProperties.background': theme.background,
         'paneProperties.vertGridProperties.color': theme.vertGridProperties,
-        // 'paneProperties.horzGridProperties.color': theme.horzGridProperties,
         'paneProperties.backgroundType': 'solid', // or 'gradient'
         'paneProperties.horzGridProperties.color': theme.horzGridProperties, // 纵轴刻度线颜色
         'paneProperties.horzGridProperties.style': 0,
         'paneProperties.crossHairProperties.color': theme.crossHairProperties, // 十字线颜色
         'paneProperties.crossHairProperties.width': 1,
         'paneProperties.crossHairProperties.style': 2,
-
         'scalesProperties.fontSize': 11,
         'scalesProperties.lineColor' : theme.lineColor, // x轴 y轴颜色
         'scalesProperties.textColor': theme.textColor, // x轴 y轴文字颜色
@@ -232,6 +191,11 @@ export default class index extends React.Component {
         'mainSeriesProperties.candleStyle.wickUpColor': theme.wickUpColor, // 蜡烛竖线颜色
         'mainSeriesProperties.candleStyle.wickDownColor': theme.wickDownColor, // 蜡烛竖线颜色
 
+        'paneProperties.legendProperties.showStudyValues': true,
+        'paneProperties.legendProperties.showSeriesTitle': false, // 是否展示 合约名称
+        'paneProperties.legendProperties.showSeriesOHLC': true,
+        'paneProperties.legendProperties.showLegend': false,
+
       },
       studies_overrides: {
         'volume.volume.color.0': theme.volume0,
@@ -239,7 +203,6 @@ export default class index extends React.Component {
       },
 		};
 
-    // this.tvWidget = new TradingView.widget(widgetOptions)
     this.tvWidget = new widget(widgetOptions);
     const tvWidget = this.tvWidget || {}
     const MA_LINES = [
@@ -250,7 +213,6 @@ export default class index extends React.Component {
 
 		tvWidget.onChartReady(() => {
 			tvWidget.headerReady().then(() => {
-        // createButton(buttons)
         MA_LINES.forEach(item => {
           this.tvWidget.chart().createStudy(
             'Moving Average',
@@ -267,7 +229,6 @@ export default class index extends React.Component {
 			});
 		});
 	}
-  
 
 	componentWillUnmount() {
 		if (this.tvWidget !== null) {
@@ -275,6 +236,7 @@ export default class index extends React.Component {
 			this.tvWidget = null;
 		}
 	}
+
   formatKlineData(ws7) {
     const klineFormatData = ws7.data.map(one => ({
       time: Number(one.time),
@@ -306,20 +268,14 @@ export default class index extends React.Component {
 
   // 加载历史k线数据
   getHistoryKLineData(firstDataRequest, resolution) {
-    const {
-      usdkData,
-      contractData,
-      type
-    } = this.props
-    // const assetId = type === '2' ? usdkData.assetId : contractData.contractId
-    const assetId = 9
+    const { webData } = this.state;
     const params = {
       reqType: 7,
       type: firstDataRequest ? 1 : 3, // 是否第一次加载历史
       param: {
         resolution: resolution === 'D' ? '1W' : resolution,
-        klineId: assetId, // 从数据来选择
-        type,
+        klineId: webData.klineId, // 从数据来选择
+        type: webData.type,
         limit: 150,
         endTime: parseInt(((firstDataRequest ? Date.now() : this.lastHistoryKlineTime) / 1000), 10),
       }
@@ -331,28 +287,21 @@ export default class index extends React.Component {
   // 订阅实时点
   subscribeRealTimeData(resolution) {
     // 插件1W周期的resolution传入的字符串为'D',针对D进行特殊处理
-    const {
-      usdkData,
-      contractData,
-      type
-    } = this.props
-    const assetId = 9
+    const { webData, type } = this.state;
     const params = {
       reqType: 7,
       type: 2,
       param: {
         resolution: resolution === 'D' ? '1W' : resolution,
-        klineId: assetId, // 从数据来选择
-        type,
+        klineId:  webData.klineId, // 从数据来选择
+        type: webData.type,
         endTime: parseInt((Date.now() / 1000), 10)
       }
     }
-    // this.props.ws.sendReq(params)
     this.props.dispatch(this.props.sendWs(this.props.wsObj, JSON.stringify(params)))
   }
 
   setSymbolName(symbolName) {
-    console.log('wocao', symbolName, this.resolution);
     try {
       this.lastHistoryKlineTime = null
       this.tvWidget.chart().setSymbol(symbolName, this.resolution, () => {})
